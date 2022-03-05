7e4e1d25de3a07421647ea592eeeddfbdfd7d8cc
Moses Burkhart <moses.burkhart1@gmail.com>
2022-03-05 16:30:01 -0500

    added public folder and middleware

--- server.js
+++ server.js
@@ -6,5 +6,12 @@ const Tweet = require('./models/tweet');
 const PORT = process.env.PORT;
 
 const app = express();
+app.engine('jsx', require('express-react-views').createEngine());
+app.set('view engine', 'jsx');
+
+app.use(morgan('tiny'));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
 
 app.listen(PORT, () => console.log('connected'));
